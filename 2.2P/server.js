6686fd218fb0442d5eb8356fc6333d4250f91fd0
dennisknu <denniswang825@gmail.com>
2025-11-21 21:59:41 +1100

    2.2P

--- 2.2P/server.js
+++ 2.2P/server.js
@@ -4,10 +4,8 @@ const path = require('path');
 const app = express();
 const PORT = 3000;
 
-// Serve public folder
 app.use(express.static(path.join(__dirname, 'public')));
 
-// GET API: /api/add?num1=5&num2=10
 app.get('/api/add', (req, res) => {
     const n1 = parseFloat(req.query.num1);
     const n2 = parseFloat(req.query.num2);
